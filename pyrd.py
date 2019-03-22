1b4a7ecedb2dc3d2d2bae6a587c4589665645d67
mwestphall <w.matthew.he@gmail.com>
2019-03-21 21:17:16 -0500

    combining too many parsers was causing stack overflow issues, so switch to regexes

--- pyrd.py
+++ pyrd.py
@@ -24,19 +24,38 @@ class Parsed():
         # default behavior: result is the parsed string
         self.result = result or parsed
 
+    def __bool__(self):
+        """Parsed is truthy if there's no error, false otherwise"""
+        return not self.error
+
     def __repr__(self):
         if self.error:
-            return self.error
+            return'Parsed(parsed="{}", left="{}", error="{}")'.format(
+                self.parsed, self.left,self.error)
         return 'Parsed(parsed="{}", left="{}", result={})'.format(
                 self.parsed, self.left,self.result)
 
+    def posIn(self,string):
+        """Work back the position in a base string based on what's left
+        to parse"""
+        if not string.endswith(self.left):
+            raise ValueError("Wrong string for parsed object")
+        consumed = string[:len(self.left)]
+        line = consumed.count('\n')+1
+        char = len(consumed) - max(consumed.rfind('\n'),0)
+        return line, char
+
+    def err(self,string):
+        line, char = self.posIn(string)
+        return "Line {}: {}".format(line,self.error)
+
+
 class Parser():
+    PARSES = 0
     def parse(self, string):
+        Parser.PARSES +=1
         """Default behavior: parse no characters successfully"""
         return Parsed(parsed="", left=string, error="")
-
-    def __call__(self,*args,**kwargs):
-        return self.parse(*args,**kwargs)
 
     def __or__(self,other):
         """ Make a ParseOr """
@@ -48,7 +67,8 @@ class Parser():
 
 class ParseOne(Parser):
     """Parse a single character off the front of the string"""
-    def parse(self,string):
+    def parse(self, string):
+        Parser.PARSES +=1
         if len(string) == 0:
             return Parsed("","","EOF")
         return Parsed(string[0],string[1:],"")
@@ -58,7 +78,8 @@ class ParseA(Parser):
     def __init__(self,chars):
         self._chars = chars
 
-    def parse(self,string):
+    def parse(self, string):
+        Parser.PARSES +=1
         if string.startswith(self._chars):
             return Parsed(self._chars,string[len(self._chars):],"")
         else:
@@ -66,60 +87,65 @@ class ParseA(Parser):
 
 class ParseAny(ParseA):
     """ Parse one of several sets of characters """
-    def parse(self,string):
+    def parse(self, string):
+        Parser.PARSES +=1
         for chars in self._chars:
             if string.startswith(chars):
                 return Parsed(chars,string[len(chars):],"")
         return Parsed("",string,"Expected one of {}".format(self._chars))
 
-class ParseNot(ParseA):
-    def parse(self,string):
-        if string.startswith(self._chars):
-            return Parsed("",string,"Expected not {}".format(self._chars))
-        else:
-            return Parsed(string[0],string[1:],"")
+class ParseRE(Parser):
+    """ Parse a regex from the front of a string """
+    def __init__(self,regex):
+        self._regex = re.compile(regex)
+
+    def parse(self, string):
+        Parser.PARSES +=1
+        match = self._regex.match(string)
+        if match:
+            return Parsed(match.group(bool(match.groups())),
+                          string[match.span()[1]:],"")
+        return Parsed("",string,"Expected match of /{}/"
+                .format(self._regex.pattern))
+
+
 
 """
-Parsers for combining other parsers
+Parsers for combining other parsers in sequence
 """
 
 class ParseObjectEither():
     """Object to store the successful result of the or'ing of parsers, if any,
     and which of the parsers succeeded"""
-    def __init__(self,result,value):
-        if isinstance(result, ParseObjectEither):
-            self.result = result.result
-            self.value = value + result.value
-        else:
-            self.result = result
-            self.value = value
+    def __init__(self,parsed,index):
+        self.result = parsed
+        self.index = index
     def __repr__(self):
-        return "Option({}: {})".format(self.value,self.result)
+        return "Option({}: {})".format(self.index,self.result)
 
 class ParseOr(Parser):
     """Parser that tries 2 parsers and succeeds if either succeeds"""
     def __init__(self, p1, p2):
-        self._p1 = p1
-        self._p2 = p2
+        self._parsers = [p1, p2]
 
-    def parse(self,string):
-        result = self._p1(string)
-        result.result = ParseObjectEither(result.result,0)
-        if result.error:
-            result = self._p2(string)
-            result.result = ParseObjectEither(result.result,1)
-        return result
+    def parse(self, string):
+        Parser.PARSES +=1
+        for i,parser in enumerate(self._parsers):
+            parsed = parser.parse(string)
+            if parsed:
+                parsed.result = ParseObjectEither(parsed.result,i)
+                return parsed
+        return parsed
 
-class ParseObjectList():
+    def __or__(self,other):
+        self._parsers.append(other)
+        return self
+
+class ParseObjectBoth():
     """Object to store the results of multiple parsers concatenated with '&'
     """
-    def __init__(self,result):
-        if isinstance(result,ParseObjectList):
-            self.results = result.results
-        elif result == PIgnore:
-            self.results = []
-        else:
-            self.results = [result]
+    def __init__(self):
+        self.results = []
 
     def append(self,result):
         if result is not PIgnore:
@@ -134,18 +160,25 @@ class ParseObjectList():
 class ParseAnd(Parser):
     """Parser that tries 2 parsers and succeeds if both succeed"""
     def __init__(self, p1, p2):
-        self._p1 = p1
-        self._p2 = p2
+        self._parsers = [p1, p2]
 
-    def parse(self,string):
-        resultl = self._p1(string)
-        if resultl.error:
-            return resultl
-        resultr = self._p2(resultl.left)
-        resultl.result = ParseObjectList(resultl.result)
-        resultl.result.append(resultr.result)
-        return Parsed(resultl.parsed + resultr.parsed,
-                      resultr.left, resultr.error,resultl.result)
+    def parse(self, string):
+        Parser.PARSES +=1
+        parsed_strings = []
+        results = ParseObjectBoth()
+        for parser in self._parsers:
+            parsed = parser.parse(string)
+            if not parsed:
+                return parsed
+            parsed_strings.append(parsed.parsed)
+            results.append(parsed.result)
+            string = parsed.left
+
+        return Parsed(''.join(parsed_strings),string,"",results)
+
+    def __and__(self,other):
+        self._parsers.append(other)
+        return self
 
 
 
@@ -157,74 +190,60 @@ class Ignore(Parser):
     def __init__(self,other):
         self._other = other
 
-    def parse(self,string):
+    def parse(self, string):
+        Parser.PARSES +=1
         result = self._other.parse(string)
         result.result = PIgnore
         return result
 
-class Maybe(Parser):
-    def __init__(self,other):
-        self._other = other
-
-    def parse(self,string):
-        return (self._other | Parser()).parse(string)
-
-class Many(Parser):
-    """Parse zero or more of an item"""
-    def __init__(self,other):
-        self._other = other
-
-    def parse(self,string):
-        result = self._other(string)
-        parsed = [result.parsed]
-        while not result.error:
-            result = self._other.parse(result.left)
-            parsed.append(result.parsed)
-        return Parsed(''.join(parsed),result.left,"")
-
-class Some(Many):
-    def parse(self,string):
-        return (self._other & Many(self._other)).parse(string)
-
 """Common utility parsers"""
-class ParseString(Parser):
-    def parse(self,string):
-        p = Ignore(ParseA('"')) & Many(ParseNot('"')) & Ignore(ParseA('"'))
-        result = p(string)
-        return result
-        
-class ParseInt(Parser):
-    def parse(self,string):
-        result = (Maybe(ParseA('-')) & Some(ParseAny(DIGITS)))(string)
-        if not result.error:
-            result.result = int(result.parsed)
-        return result
-
-class ParseFloat(Parser):
-    def parse(self,string):
-        p = Maybe(ParseA('-')) & (
-            (Many(ParseAny(DIGITS)) & ParseA('.') & Some(ParseAny(DIGITS))) | 
-            (Some(ParseAny(DIGITS)) & ParseA('.') & Many(ParseAny(DIGITS))))
-        result = p(string)
-        if not result.error:
-            result.result = float(result.parsed)
-        return result
-
-class ParseBool(ParseAny):
+class ParseString(ParseRE):
+    STRING_RE = re.compile(r'"([^"]*)"')
     def __init__(self):
-        self._chars = ["true","false"]
+        self._regex = self.STRING_RE
+
+class ParseInt(ParseRE):
+    INT_RE = re.compile(r'-?[0-9]+')
+    def __init__(self):
+        self._regex = self.INT_RE
+    def parse(self, string):
+        Parser.PARSES +=1
+        result = super().parse(string)
+        if result:
+            result.result = int(result.result)
+        return result
+
+class ParseFloat(ParseRE):
+    FLOAT_RE = re.compile(r'-?[0-9]*\.?[0-9]+')
+    def __init__(self):
+        self._regex = self.FLOAT_RE
+    def parse(self, string):
+        Parser.PARSES +=1
+        result = super().parse(string)
+        if result:
+            result.result = float(result.result)
+        return result
+
+class ParseBool(ParseRE):
+    BOOL_RE = re.compile(r'(true|false)')
+    def __init__(self):
+        self._regex = BOOL_RE
 
 class Spaces(Parser):
-    def parse(self,string):
-        p = Ignore(Many(ParseAny(' \t\n')))
-        return p(string)
+    def parse(self, string):
+        Parser.PARSES +=1
+        p = Ignore(ParseRE(r'[ \t\n]*'))
+        return p.parse(string)
 
 class SpacesAround(Parser):
     def __init__(self,other):
         self._other = other
-    def parse(self,string):
+
+    def parse(self, string):
+        Parser.PARSES +=1
         p = Spaces() & self._other & Spaces()
-        return p(string)
+        return p.parse(string)
 
 if __name__ == '__main__':
-    print((ParseFloat())('-5.'))
+    p = ParseA('[') & ParseInt() & ParseA(']')
+    print(p,p._parsers,p.parse("[5]"))
